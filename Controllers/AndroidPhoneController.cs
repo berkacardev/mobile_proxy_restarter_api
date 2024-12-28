14815ee29b923a74b4d54bc50f885f8cf2ead664
Berk Acar <berkacar.dev@gmail.com>
2024-12-28 13:36:07 +0300

    first commit

---
+++ Controllers/AndroidPhoneController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobileProxyApi.Helper;
+
+namespace MobileProxyApi.Controllers
+{
+    public class AndroidPhoneController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult DisableAirplaneMode()
+        {
+            try
+            {
+                AdbHelper.StartProcess();
+                return Ok(new { message = "Ip Changed" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred.", error = ex.Message });
+            }
+        }
+    }
+}
+
---
+++ Helper/AdbHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace MobileProxyApi.Helper
+{
+    public static class AdbHelper
+    {
+        private static void RunAdbCommand(string command, string workingDirectory = "platform-tools")
+        {
+            Process process = new Process();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.WorkingDirectory = "C:\\platform-tools";
+            process.StartInfo.Arguments = "/c " + command;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        public static void StartProcess()
+        {
+            RunAdbCommand("adb shell svc data disable");
+            RunAdbCommand("adb shell svc data enable");
+        }
+    }
+}
